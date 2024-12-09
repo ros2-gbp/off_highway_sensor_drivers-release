9548fcdc19c7edfb521a58702666496c47f6403a
Robin Petereit <robin.petereit@de.bosch.com>
2024-12-09 09:00:52 +0100

    Rebase from 'debian/humble/off_highway_can'

--- src/can_message.cpp
+++ src/can_message.cpp
@@ -13,12 +13,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <stdexcept>
+
 #include "off_highway_can/can_message.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 
 namespace off_highway_can
 {
+
+template<>
+void Signal::decode(const ros2_socketcan_msgs::msg::FdFrame::_data_type & frame)
+{
+  // Needed due to https://github.com/ros2/rosidl/issues/799
+  const uint8_t * data = static_cast<const uint8_t *>(frame.data<void>());
+  value = ::decode(data, start_bit, length, is_big_endian, is_signed, factor, offset);
+}
+
+template<>
+void Signal::encode(ros2_socketcan_msgs::msg::FdFrame::_data_type & frame)
+{
+  // Needed due to https://github.com/ros2/rosidl/issues/799
+  uint8_t * data = static_cast<uint8_t *>(frame.data<void>());
+  ::encode(data, value, start_bit, length, is_big_endian, is_signed, factor, offset);
+}
+
+template<>
+uint8_t Message::calculate_crc(const ros2_socketcan_msgs::msg::FdFrame::_data_type & /*frame*/)
+{
+  // TODO(rcp1-beg) Implement CRC for FD frames
+  throw std::runtime_error("CRC calculation for FD frames not implemented!");
+  return 0xFF;
+}
+
 bool Signal::set(double value, const std::string & signal_name)
 {
   if (value < min || value > max) {
